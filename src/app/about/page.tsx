a962b60dfa7ccc5482204dc55a65cab8f28bde14
Ajay <iamask@live.com>
2025-01-06 15:04:49 +0530

    commit to about-page branch

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -77,7 +77,7 @@ export default async function AboutPage() {
 
         <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
           <a
-            href="https://nextjs.org/docs"
+            href="THis is about pahe"
             className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
             target="_blank"
             rel="noopener noreferrer"
