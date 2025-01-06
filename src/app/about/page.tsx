e98d9f9043d28e49856d11803f04ceb24675f0c0
Ajay <iamask@live.com>
2025-01-06 13:08:08 +0530

    updated pages

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,16 @@
-export default function AboutPage() {
+interface ApiResponse {
+  message: string;
+}
+
+export default async function AboutPage() {
+    // Fetch data from an external API
+    const response = await fetch('https://pokeapi.co/api/v2/version-group/5/', { cache: 'no-store' }); // no-store ensures fresh data
+    const data: ApiResponse = await response.json();
+  
     return (
       <main>
         <h1>About Us</h1>
-        <p>This is the About page.</p>
+        <p>Message from API: {data.message}</p>
       </main>
     );
   }
